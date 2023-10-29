1d90328ad82b62a56af75df7d6b9728cde98b30c
tesla8877 <ishihara.yuudai727@gmail.com>
2023-10-29 21:02:57 +0900

    update:

--- app/src/main/java/com/tesla/obs/StayService.java
+++ app/src/main/java/com/tesla/obs/StayService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.tesla.obs.Helper;
@@ -14,16 +15,21 @@ public class StayService extends Service {
 
     @Override
     public void onCreate() {
-        if(Helper.isTargetRunning(this, String.valueOf(R.string.target_app))) {
-            Toast.makeText(this, String.valueOf(R.string.already_running), Toast.LENGTH_LONG);
+        Log.d("T", "StayService.onCreate executed");
+        if(Helper.isTargetRunning(getApplicationContext(), getResources().getString(R.string.target_app))) {
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.already_running), Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this, String.valueOf(R.string.app_down), Toast.LENGTH_LONG);
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.app_down), Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
+        if(Helper.isTargetRunning(getApplicationContext(), getResources().getString(R.string.target_app))) {
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.already_running), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.app_down), Toast.LENGTH_SHORT).show();
+        }
         return START_STICKY;
     }
 
